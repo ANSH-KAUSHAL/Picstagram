73920b0c158f1fd077e4e4b445e4697699a781ba
ANSH-KAUSHAL <kaushalansh777@gmail.com>
2024-11-12 14:59:41 +0530

    feed fixed

--- index.js
+++ index.js
@@ -1,25 +1,54 @@
-var modal = document.getElementById("modal");
-var span = document.getElementsByClassName("close")[0];
+document.addEventListener("DOMContentLoaded", function () {
+  let phoneInput = document.getElementById("input");
+  let errorDiv = document.getElementById("error");
+  let form = document.getElementById("signup-form");
+  let submitBtn = document.getElementById("btn");
 
-window.onload = function () {
-  if (window.innerWidth >= 1024) {
-    modal.style.display = "block";
-    let storedUserData = JSON.parse(localStorage.getItem("userData"));
-    if (storedUserData && storedUserData.firstName) {
-      let msg = document.getElementById("welcome-msg");
-      msg.innerText = "Welcome " + storedUserData.firstName + "❤️";
+  if (!form || !phoneInput || !errorDiv) {
+    console.error("One or more elements are missing");
+    return;
+  }
+
+  phoneInput.onblur = function () {
+    if (phoneInput.value.length !== 10) {
+      phoneInput.classList.add("invalid");
+      errorDiv.innerText = "Phone Number must be 10 digits.";
+      submitBtn.disabled = true;
+    } else {
+      submitBtn.disabled = false;
+      errorDiv.innerText = "";
     }
-  } else {
-    modal.style.display = "none";
-  }
-};
+  };
 
-span.onclick = function () {
-  modal.style.display = "none";
-};
+  phoneInput.onfocus = function () {
+    if (this.classList.contains("Invalid")) {
+      this.classList.remove("Invalid");
+      errorDiv.innerHTML = "";
+    }
+  };
 
-window.onclick = function (event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-};
+  form.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const firstName = document.getElementById("first-name").value;
+    const lastName = document.getElementById("last-name").value;
+    const username = document.getElementById("username").value;
+    const phone = phoneInput.value;
+    const dob = document.getElementById("dob").value;
+    const password = document.getElementById("password").value;
+
+    const user = {
+      firstName,
+      lastName,
+      username,
+      phone,
+      dob,
+      password,
+    };
+
+    localStorage.setItem("userData", JSON.stringify(user));
+    console.log(user);
+
+    window.location.href = "login.html";
+  });
+});
